cf9f3265367387f59102f22645ee05e8d32c1317
Maxime Moreillon <m.moreillon@gmail.com>
2024-10-16 06:42:12 +0900

    adapted for new timescaledb backend

--- registration.ts
+++ registration.ts
@@ -3,18 +3,23 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const { FINANCES_API_TOKEN, FINANCES_API_URL, FINANCES_API_ACCOUNT_NAME } =
+const { FINANCES_API_TOKEN, FINANCES_API_URL, FINANCES_API_ACCOUNT } =
   process.env
 
-export const register_transactions = (transactions: any[]) => {
+export const register_transactions = async (transactions: any[]) => {
   console.log(`[Finances API] Registering transactions`)
 
-  const url = `${FINANCES_API_URL}/accounts/${FINANCES_API_ACCOUNT_NAME}/transactions`
-  const body = { transactions }
+  const url = `${FINANCES_API_URL}/accounts/${FINANCES_API_ACCOUNT}/transactions`
   const headers = {
     "Content-Type": "application/json",
     Authorization: `Bearer ${FINANCES_API_TOKEN}`,
   }
 
-  return axios.post(url, body, { headers })
+  // const body = { transactions }
+  // return axios.post(url, body, { headers })
+
+  for (const { date, description, amount } of transactions) {
+    const body = { time: date, description, amount }
+    await axios.post(url, body, { headers })
+  }
 }
